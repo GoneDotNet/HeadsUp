e01730b2954b924661cfe592d2635bfdff38d1b5
Allan Ritchie <allan.ritchie@gmail.com>
2026-03-13 00:26:52 -0400

    Update Constants.cs

--- GoneDotNet.HeadsUp/Constants.cs
+++ GoneDotNet.HeadsUp/Constants.cs
@@ -4,10 +4,11 @@ namespace GoneDotNet.HeadsUp;
 public static class Constants
 {
     public const int MaxAnswersPerGame = 25;
+    public const int MaxAnswersPerCategory = 100;
     public const string AzureOpenAiModel = "gpt-4.1";
 #if DEBUG
-    public const string AzureOpenAiApiKey = "FRiLwR72ZuvOWu58pjk1E77ifEb5JpKl0oAiw5u1tsadvEzJkaR9JQQJ99BJACYeBjFXJ3w3AAABACOGr6Bu";
-    public const string AzureOpenAiEndpoint = "https://shinyopenai.openai.azure.com/";
+    public const string AzureOpenAiApiKey = "";
+    public const string AzureOpenAiEndpoint = "";
 #else
     public const string AzureOpenAiApiKey = "${OPENAI_API_KEY}";
     public const string AzureOpenAiEndpoint = "{OPENAI_ENDPOINT}";
