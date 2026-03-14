870afbe62041c34cecf857b8a21628184113d674
Allan Ritchie <allan.ritchie@gmail.com>
2026-03-13 21:20:16 -0400

    For release

--- GoneDotNet.HeadsUp/Constants.cs
+++ GoneDotNet.HeadsUp/Constants.cs
@@ -5,12 +5,4 @@ public static class Constants
 {
     public const int MaxAnswersPerGame = 25;
     public const int MaxAnswersPerCategory = 100;
-    public const string AzureOpenAiModel = "gpt-4.1";
-#if DEBUG
-    public const string AzureOpenAiApiKey = "";
-    public const string AzureOpenAiEndpoint = "";
-#else
-    public const string AzureOpenAiApiKey = "${OPENAI_API_KEY}";
-    public const string AzureOpenAiEndpoint = "{OPENAI_ENDPOINT}";
-#endif
 }
