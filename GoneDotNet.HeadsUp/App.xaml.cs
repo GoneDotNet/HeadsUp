cc69c56c9b146efdda957dcb48ab38c9688bb04d
Allan Ritchie <allan.ritchie@gmail.com>
2026-04-05 12:33:44 -0400

    Updates

--- GoneDotNet.HeadsUp/App.xaml.cs
+++ GoneDotNet.HeadsUp/App.xaml.cs
@@ -2,14 +2,7 @@
 
 public partial class App : Application
 {
-    public App()
-    {
-        this.InitializeComponent();
-    }
-
-
+    public App() => this.InitializeComponent();
     protected override Window CreateWindow(IActivationState? activationState)
-    {
-        return new Window(new AppShell());
-    }
+        => new(new AppShell());
 }
--- GoneDotNet.HeadsUp/AppShell.xaml.cs
+++ GoneDotNet.HeadsUp/AppShell.xaml.cs
@@ -1,6 +1,6 @@
 namespace GoneDotNet.HeadsUp;
 
-public partial class AppShell : Shell
+public partial class AppShell : ShinyShell
 {
     public AppShell()
     {
